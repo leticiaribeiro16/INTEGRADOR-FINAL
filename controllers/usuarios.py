4be06422e2a0be545c5b25ad29aafeeb5c5f59ec
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2023-01-20 14:59:13 +0000

    crud usuario e demanda completo, admin e user comum definidos

--- controllers/usuarios.py
+++ controllers/usuarios.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, Blueprint
+from flask import render_template, request, redirect, Blueprint, url_for
 from database.models import Usuario
 from database.database import db, lm
 from flask_login import login_user, current_user, logout_user, login_required
@@ -24,8 +24,49 @@ def cadastro():
 
 @bp_usuarios.route('/recovery')
 def recovery():
-	usuarios = Usuario.query.all()
-	return render_template('recovery.html', usuarios = usuarios)
+  if not current_user.admin:
+    return redirect('/login')
+    
+  usuarios = Usuario.query.all()
+  return render_template('recovery.html', usuarios = usuarios)
+
+@bp_usuarios.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+  usuario = Usuario.query.get(id)
+  if id and request.method=='GET':
+    return render_template('usuarios_update.html', usuario=usuario)
+    
+  if request.method=='POST':
+    nome = request.form.get('nome')
+    email = request.form.get('email')
+    matricula = request.form.get('matricula')
+    senha = request.form.get('senha')
+    admin = request.form.get('admin')
+    
+    usuario.nome = nome
+    usuario.email = email
+    usuario.matricula = matricula
+    usuario.senha = senha
+    usuario.admin = eval(admin)
+
+    db.session.add(usuario) 
+    db.session.commit()
+    return redirect(url_for('.recovery', id=id))
+
+@bp_usuarios.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+  if id==0:
+    return redirect(url_for('usuarios'))
+
+  if request.method == 'GET':
+    usuario = Usuario.query.get(id)
+    return render_template('usuarios_delete.html', usuario = usuario)
+
+  if request.method == 'POST':
+    usuario = Usuario.query.get(id)
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect(url_for('.recovery'))
 
 @lm.user_loader
 def load_user(id):
